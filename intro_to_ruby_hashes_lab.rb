b174a684ec9b213d8c31c5be188f11f024d4bbe1
Victoria Cushing <victoriaccushing@gmail.com>
2019-07-08 21:30:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,6 +25,9 @@ def monopoly_with_third_tier
 	      },
 	      :pennsylvania_railroad => {
 	        
+	      },
+	      :b_and_o_railroad => {
+	        
 	      }
 	    },
 	    :rent_in_dollars => {
