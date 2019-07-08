83c37819c5041bd3fc1947a2f0c960c1188ebd35
Victoria Cushing <victoriaccushing@gmail.com>
2019-07-08 21:22:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,9 @@ def monopoly_with_second_tier
 	  :railroads => {
 	    :pieces => 4,
 	    :names => {},
-	    :rent_in_dollars => {}
+	    :rent_in_dollars => {
+	      :one_piece_owned => 25
+	    }
 	  }
 	}
 end
